d398b9ccc6dd981d4b7394bc0e7d3b7cff1ad13e
kouchou <kouchou@>
2018-06-15 04:55:16 +0900

    [Update] コードレビューに従いました
    - 変数名を適切に変更
    - 機能を関数にまとめた
    - 不要なif文を削除

--- main.js
+++ main.js
@@ -1,43 +1,39 @@
 // 初期配置の配列
-var board = [
-  [{"owner": 1, "kind": "KY"}, {"owner": 1, "kind": "KE"}, {"owner": 1, "kind": "GI"}, {"owner": 1, "kind": "KI"}, {"owner": 1, "kind": "OU"}, {"owner": 1, "kind": "KI"}, {"owner": 1, "kind": "GI"}, {"owner": 1, "kind": "KE"}, {"owner": 1, "kind": "KY"}],
-  [{}, {"owner": 1, "kind": "HI"}, {}, {}, {}, {}, {}, {"owner": 1, "kind": "KA"}, {}],
-  [{"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}, {"owner": 1, "kind": "FU"}],
+var initPosition = [
+  [{"owner": "black", "kind": "KY"}, {"owner": "black", "kind": "KE"}, {"owner": "black", "kind": "GI"}, {"owner": "black", "kind": "KI"}, {"owner": "black", "kind": "OU"}, {"owner": "black", "kind": "KI"}, {"owner": "black", "kind": "GI"}, {"owner": "black", "kind": "KE"}, {"owner": "black", "kind": "KY"}],
+  [{}, {"owner": "black", "kind": "HI"}, {}, {}, {}, {}, {}, {"owner": "black", "kind": "KA"}, {}],
+  [{"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}, {"owner": "black", "kind": "FU"}],
   [{}, {}, {}, {}, {}, {}, {}, {}, {}],
   [{}, {}, {}, {}, {}, {}, {}, {}, {}],
   [{}, {}, {}, {}, {}, {}, {}, {}, {}],
-  [{"owner": 0, "kind": "KY"}, {"owner": 0, "kind": "KE"}, {"owner": 0, "kind": "GI"}, {"owner": 0, "kind": "KI"}, {"owner": 0, "kind": "OU"}, {"owner": 0, "kind": "KI"}, {"owner": 0, "kind": "GI"}, {"owner": 0, "kind": "KE"}, {"owner": 0, "kind": "KY"}],
-  [{}, {"owner": 0, "kind": "HI"}, {}, {}, {}, {}, {}, {"owner": 0, "kind": "KA"}, {}],
-  [{"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}, {"owner": 0, "kind": "FU"}]
+  [{"owner": "white", "kind": "KY"}, {"owner": "white", "kind": "KE"}, {"owner": "white", "kind": "GI"}, {"owner": "white", "kind": "KI"}, {"owner": "white", "kind": "OU"}, {"owner": "white", "kind": "KI"}, {"owner": "white", "kind": "GI"}, {"owner": "white", "kind": "KE"}, {"owner": "white", "kind": "KY"}],
+  [{}, {"owner": "white", "kind": "HI"}, {}, {}, {}, {}, {}, {"owner": "white", "kind": "KA"}, {}],
+  [{"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}, {"owner": "white", "kind": "FU"}]
 ]
 
+var createKoma = function(position) {
+  // #boardのelementを取得
+  var $Board = document.getElementById("board");
 
-// #boardのelementを取得
-var $Board = document.getElementById("board");
-
-// パフォーマンスのためにfragmentを使って1回のDOMアクセスにする
-
-// fragmentをつくる
-var fragment = document.createDocumentFragment();
-
-// fragmentに要素を入れていく
-board.forEach(function(row, index) {
-  row.forEach(function(cell, index) {
-    var $element = document.createElement("div");
-    $element.classList.add("cell");
-    if (!Object.keys(cell).length) {
-      fragment.appendChild($element);
-      return;
-    }
-    if (cell["owner"] === 0) {
-      $element.classList.add("white");
-    } else {
-      $element.classList.add("black");
-    }
-    $element.classList.add(cell["kind"]);
-    fragment.appendChild($element);
+  // パフォーマンスのためにfragmentを使って1回のDOMアクセスにする
+  // fragmentをつくる
+  var komaFragment = document.createDocumentFragment();
+  // fragmentに要素を入れていく
+  position.forEach(function(row, index) {
+    row.forEach(function(cell, index) {
+      var $element = document.createElement("div");
+      $element.classList.add("cell");
+      if (!Object.keys(cell).length) {
+        komaFragment.appendChild($element);
+        return;
+      }
+      $element.classList.add(cell["owner"]);
+      $element.classList.add(cell["kind"]);
+      komaFragment.appendChild($element);
+    });
   });
-});
+  // boardの子要素に追加する
+  $Board.appendChild(komaFragment);
+}
 
-// boardの子要素に追加する
-$Board.appendChild(fragment);
+createKoma(initPosition);
