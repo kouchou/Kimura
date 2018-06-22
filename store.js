4a952c7eb71fe166ffbeb05f8f45fb398772e1f4
kouchou <kouchou@>
2018-06-23 03:02:24 +0900

    [Add] 新たに盤面情報と手駒情報を持つステータスを追加

--- store.js
+++ store.js
@@ -1,16 +1,27 @@
 // ストア
-store = new Object();
-// 棋譜
-// [{"count": int, "owner": string, "kind": string, "from": [int, int], "to": [int, int], "nari": bool}, {...}]
-store.kifu = [];
-store.addKifu = function(owner, kind, from, to, nari) {
-  if (store.kifu.length > 0) {
-    var count = store.kifu[store.kifu.length - 1] + 1;
-    store.kifu.push({"count": count, "owner": owner, "kind": kind, "from": from, "to": to, "nari": nari});
-  } else {
-    store.kifu.push({"count": 1, "owner": owner, "kind": kind, "from": from, "to": to, "nari": nari});
-  }
+var store = new Object();
+// 盤面
+store.board = null;
+store.getBoard = function() {
+  return store.board;
 };
+store.setBoard = function(board) {
+  store.board = board;
+};
+// 持ち駒
+store.hands = {
+  "white": {"HI": 0, "KA": 0, "KI": 0, "GI": 0, "KE": 0, "KY": 0, "FU": 0},
+  "black": {"HI": 0, "KA": 0, "KI": 0, "GI": 0, "KE": 0, "KY": 0, "FU": 0}
+};
+store.getHands = function(owner) {
+  return store.hands[owner];
+};
+store.addHands = function(owner, kind) {
+  store.hands[owner][kind]++;
+};
+store.removeHands = function(owner, kind) {
+  store.hands[owner][kind]--;
+}
 // 手番 String
 store.turn = "white";
 store.getTurn = function() {
@@ -41,4 +52,15 @@ store.selectKoma = function($Element) {
 };
 store.unselectKoma = function() {
   store.selectedKoma = null;
+};
+// 棋譜
+// [{"count": int, "owner": string, "kind": string, "from": [int, int], "to": [int, int], "nari": bool}, {...}]
+store.kifu = [];
+store.addKifu = function(owner, kind, from, to, nari) {
+  if (store.kifu.length > 0) {
+    var count = store.kifu[store.kifu.length - 1] + 1;
+    store.kifu.push({"count": count, "owner": owner, "kind": kind, "from": from, "to": to, "nari": nari});
+  } else {
+    store.kifu.push({"count": 1, "owner": owner, "kind": kind, "from": from, "to": to, "nari": nari});
+  }
 };
